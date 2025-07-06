d3da9ce03c77010beb66f62b42e7b639e3e32c9f
danishrahmankpm <140050542+danishrahmankpm@users.noreply.github.com>
2025-07-06 16:28:50 +0530

    Initial commit

---
+++ src/main/java/com/sample/PromoEngine/PromoEngineApplication.java
@@ -0,0 +1,13 @@
+package com.sample.PromoEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/PromoEngine/PromoEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.PromoEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
